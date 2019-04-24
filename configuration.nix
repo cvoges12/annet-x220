6b44aeb26673ef58879a0ed71d5747a685bc8a5b
Clayton Voges <38054771+cvoges12@users.noreply.github.com>
2019-04-24 09:17:13 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,10 +4,10 @@
   imports = [ 
     <nixos-hardware/lenovo/thinkpad/x220>
     ./boot.nix
-    ./common/import.nix
     ./fileSystems.nix
     ./networking.nix
     ./swapDevices.nix
+    (import (builtins.fetchGit https://github.com/cvoges12/annet-common) {})
     (import (builtins.fetchGit https://github.com/cvoges12/annet-personal) {})
   ];
 }
