528f1050c08b231110a445f09bd93048d6001056
Clayton Voges <vogesclayton@gmail.com>
2019-05-06 19:43:24 -0500

    Added rev and ref to fetchGit

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,14 @@
     ./fileSystems.nix
     ./networking.nix
     ./swapDevices.nix
-    (import (builtins.fetchGit https://github.com/cvoges12/annet-common) {})
-    (import (builtins.fetchGit https://github.com/cvoges12/annet-personal) {})
+    (import (builtins.fetchGit {
+      url = "https://github.com/cvoges12/annet-common";
+      ref = "master";
+    }) {})
+    (import (builtins.fetchGit {
+      url = "https://github.com/cvoges12/annet-personal";
+      ref = "master";
+      rev = "1abdbe1a6390c264800b46d4c41357c18710f1cf";
+    }) {})
   ];
 }
