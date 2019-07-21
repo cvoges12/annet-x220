7db88149168c39678839193e6b7ea3dcebe53246
Clayton Voges <vogesclayton@gmail.com>
2019-07-21 15:06:20 -0500

    update

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }: 
 
-{ 
+{
   imports = [ 
     <nixos-hardware/lenovo/thinkpad/x220>
     ./boot.nix
@@ -10,11 +10,10 @@
     (import (builtins.fetchGit {
       url = "https://gitlab.com/cvoges12/annet-common";
       ref = "master";
-    }) {})
+      rev = "31bb03e95cfef501cc307f114c2a32846810002b";}) {})
     (import (builtins.fetchGit {
       url = "https://gitlab.com/cvoges12/annet-personal";
       ref = "master";
-      rev = "d18def567fb7c1f3ae97c66fb79f22b6c5f52938";
-    }) {})
+      rev = "c88879df2bac57adc94f506ed7b798f4278f7cd8";}) {})
   ];
 }
